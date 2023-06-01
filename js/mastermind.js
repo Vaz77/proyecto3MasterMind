304685104ba194902a125c5c2fdfa15530e2ae9c
Jesus <jesusvazquez2477@gmail.com>
2023-06-01 23:48:59 +0200

    Modifico archivo mastermind.js para hacer las funciones para que al hacer click en el boton check continuemos en la siguiente fila y bloquee la anterior(No funciona), cambio y elimino fondos, modifico enlaces para pages

--- js/mastermind.js
+++ js/mastermind.js
@@ -2,37 +2,28 @@
 const colorArray = JSON.parse(sessionStorage.getItem("colorsArr"));
 // CON JSON.STRINGIFY CONVERTIMOS A STRING CUALQUIER COSA PARA GUARDARLOS EN EL SESSION STORAGE
 // CON JSON.PARSE VOLVEMOS A CONVERTIRLO EN EL TIPO DE JAVASCRIPT, OBJETO... ARRAY.... NUMERO...
-
 // CREACIÓN DE VARIABLES / CONSTANTES
 const lvlMode = "easy";
-const eleccionUsuario = (lvlMode === "easy") ? [A, B, C, D] : (lvlMode === "medium") ? [A, B, C, D, E] : [A, B, C, D, E, F];// ARRAY QUE GUARDARÁ EL COLOR QUE ELIJA EL USUSARIO EN LA POSICIÓN DE LA BOLA QUE LO ELIJA
-const combinacionSecreta = []; // CREAR ARRAY PARA CONBINACIÓN SECRETA DE COLORES
-const comprobar = document.getElementById("btnCheck"); // CAPTURAR BOTON DE CHECK
-const bolitasCheck = document.querySelectorAll(".fila1 > .check > .d-flex > .check-circle");  // CAPTURAR LAS BOLITAS PEQUEÑAS DEL CHECK
+const eleccionUsuario = [];
+//ARRAY QUE GUARDARÁ EL COLOR QUE ELIJA EL USUSARIO EN LA POSICIÓN DE LA BOLA QUE LO ELIJA
+const combinacionSecreta = [];
+const comprobar = document.getElementById("btnCheck");
+const bolitasCheck = document.querySelectorAll(".fila1 > .check > .d-flex > .check-circle");
 const filaArray = Array.from(document.querySelectorAll(".fila")).reverse();
 console.log("array filas :", filaArray);
 
 
 
-
-
-
-
-// GENERAR LA CONBINACIÓN SECRETA DE LA PARTIDA
+let cuentaFilas = 1;
 const creaCombinacionSecreta = () => {
   for (i = 0; i < 4; i++) {
     let numeroAleatorio = Math.floor(Math.random() * colorArray.length);
     combinacionSecreta.push(colorArray[numeroAleatorio]);
   }
 };
-
 // EJECUTAR LA FUNCIÓN
 creaCombinacionSecreta();
-console.log("este es el codigo secreto :" +combinacionSecreta);
-
-
-
-
+console.log("este es el codigo secreto :" + combinacionSecreta);
 //EJECUTAR SELECCION DEL JUGADOR
 const playerSelectColor = (fila) => {
   const eleccionColor = fila.querySelectorAll(".ficha");
@@ -40,9 +31,8 @@ const playerSelectColor = (fila) => {
   eleccionColor.forEach((eleccionColor, index) => {
   let posicion = 0;
   eleccionColor.addEventListener("click", () => {
-    // AL HACER CLICK ASIGNAMOS EL COLOR
     eleccionColor.style.backgroundColor = colorArray[posicion];
-    // Y AGREGAMOS EL COLOR QUE HA ELEGIDO EL USUARIO A NUESTRO ARRAY FINAL
+    //AGREGAMOS EL COLOR QUE HA ELEGIDO EL USUARIO A NUESTRO ARRAY FINAL
     eleccionUsuario[index] = colorArray[posicion];
     if (posicion > colorArray.length){
       posicion = 0;
@@ -54,8 +44,37 @@ const playerSelectColor = (fila) => {
 
 
 
-// CAPTURAR BOTON DE VALIDAR, Y AÑADIR EVENTO CLICK
 comprobar.addEventListener("click", () => {
+  document.getElementsByClassName(`fila${cuentaFilas}`).classList.toggle('filaInactiva');
+  eleccionUsuario.forEach((eleccion, index) => {
+    if (eleccion === combinacionSecreta[index]) {
+      console.log("son iguales");
+      bolitasCheck[index].style.backgroundColor = "#000000";
+    } else if (combinacionSecreta.includes(eleccion)) {
+      console.log("no es igual pero existe");
+      bolitasCheck[index].style.backgroundColor = "#ffffff";
+    } else {
+      bolitasCheck[index].style.backgroundColor = "#2bff00";
+      console.log("no son iguales");
+    }
+  });
+
+  cuentaFilas++;
+});
+
+
+
+/* CAPTURAR BOTON DE VALIDAR, Y AÑADIR EVENTO CLICK
+comprobar.addEventListener("click", () => {
+  let cuentaFilas = 1;
+  let fila = document.getElementsByClassName(`fila${cuentaFilas}`)
+  console.log (`fila${cuentaFilas}`)
+  console.log("Esta es la variable fila" + fila)
+  fila.classList.add("filaInactiva");
+  cuentaFilas++
+  fila = document.getElementsByClassName(`fila${cuentaFilas}`)
+  
+  fila.classList.remove("filaInactiva");
 
   eleccionUsuario.forEach((eleccion, index) => {
     if (eleccion === combinacionSecreta[index]) {
@@ -73,19 +92,15 @@ comprobar.addEventListener("click", () => {
     }
   });
 });
-
+*/
 
 
 //ITERACION FILAS
-let cuentaFilas = 1;
-
-
 const cambioFilas = () => {
   if(cuentaFilas <= 10 ) {
       filaArray.forEach((fila, index) => {
     let filaActiva = document.querySelector(`.fila${cuentaFilas}`)
     playerSelectColor(filaActiva)
-
         //BOTONCHECK
           //COMPROBAR SI HA GANADO
             //SI HA GANADO: 
@@ -99,9 +114,6 @@ const cambioFilas = () => {
                   //CUENTAFILAS++
                 //NO HAY VALORES 
                   //RETURN
-
-
-
     cuentaFilas++
   }) 
   }
@@ -119,55 +131,26 @@ cambioFilas()
 
 
 
-
-
 /*
-// OBTENER LA DIFICULTAD ALMACENADA EN EL SESSIONSTORAGE
-const dificultadSeleccionada = sessionStorage.getItem("dificultad");
+//POSIBLE TABLERO DINAMICO
+const iniciarJuego = (nivel) => {
+  let numeroFichas = 4;
 
-// FUNCIÓN PARA GENERAR EL TABLERO SEGÚN LA DIFICULTAD SELECCIONADA
-const generarTablero = (dificultad) => {
-  let numFilas;
-  let numFichas;
+  if (nivel === 'medio') {
+    numeroFichas = 5;
+  } else if (nivel === 'dificil') {
+    numeroFichas = 7;
+  } 
 
-  // DETERMINAR EL NÚMERO DE FILAS Y FICHAS SEGÚN LA DIFICULTAD SELECCIONADA
-  if (dificultad === "facil") {
-    numFilas = 10;
-    numFichas = 4;
-  } if (dificultad === "medio") {
-    numFilas = 8;
-    numFichas = 5;
-  } else if (dificultad === "dificil") {
-    numFilas = 6;
-    numFichas = 6;
+  const combinacion = document.querySelector('.combinacion');
+  combinacion.innerHTML = '';
+
+  for (let i = 0; i < numeroFichas; i++) {
+    const ficha = document.createElement('div');
+    ficha.classList.add('ficha');
+    combinacion.appendChild(ficha);
   }
 
-  // LIMPIAR EL TABLERO EXISTENTE
-  const tablero = document.querySelector(".tablero");
-  tablero.innerHTML = "";
-
-  // GENERAR LAS FILAS Y FICHAS DINÁMICAMENTE
-  for (let i = 1; i <= numFilas; i++) {
-    const fila = document.createElement("div");
-    fila.classList.add("fila", `fila${i}`, "d-flex", "align-items-center", "justify-content-center", "mb-3");
-
-    const checkBtn = document.createElement("button");
-    checkBtn.id = `btnCheck${i}`;
-    checkBtn.classList.add("btn", "btn-primary");
-    checkBtn.textContent = "Check";
-
-    fila.appendChild(checkBtn);
-
-    for (let i = 0; i < numFichas; i++) {
-      const ficha = document.createElement("div");
-      ficha.classList.add("ficha");
-      fila.appendChild(ficha);
-    }
-
-    tablero.appendChild(fila);
-  }
 };
 
-// LLAMAR A LA FUNCIÓN INICIALMENTE PARA GENERAR EL TABLERO DE DIFICULTAD SELECCIONADA
-generarTablero(dificultadSeleccionada);
-*/
+ */
